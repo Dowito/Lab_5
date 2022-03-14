20483b1d264c0405866cb3b90760e3d0e698200d
David <david.gonzalez12@gmail.com>
2022-03-13 19:10:06 -0500

    Version estable.

--- enemigo.h
+++ enemigo.h
@@ -16,12 +16,7 @@ public:
     void setVel(unsigned short newVel);
 
 private:
-    /*!
-     * \brief estado si esta vivo o muerto, morira si es tocado por la explosion de una bomba.
-     * al morir, se reproduce la animacion de muerte. y mientras este en false, no aparecera en pantalla
-     */
     bool state; //por defecto true
-
     unsigned short vel; //la velocidad sera un parametro del constructor, asi se crearan diferentes enemigos
 };
 
--- personaje.h
+++ personaje.h
@@ -22,22 +22,9 @@ public:
     void setVel(unsigned char newVel);
 
 private:
-    /*!
-     * \brief estado si esta vivo o muerto, cambiara el estado si es tocado por la explosion de una bomba, si se acaba el
-     * tiempo o si es tocado por un enemigo.
-     */
     bool estado; //por defecto true
-
     unsigned char lifes; //vidas faltantes antes de terminar el juego
-    /*!
-     * \brief bombs cantidad de bombas que puede poner simultaneamente.
-     * aumentara la cantidad a medida que vaya recolectando aumentos.
-     */
     unsigned char bombs; //defecto 1
-    /*!
-     * \brief vel velocidad con la que se mueve el personaje.
-     * aumentara a medida que vaya recolectando aumentos.
-     */
     unsigned char vel; //por defecto 2
 
 };
